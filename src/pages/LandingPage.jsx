184693945d669831992ef5c5498ff9e307c75eab
Abdulbasit <abdulbasitkayode@gmail.com>
2026-01-18 21:34:15 +0100

    Update LandingPage to include main sections

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Hero from '../components/sections/Hero/Hero'
+import Features from '../components/sections/Features/Features'
+import About from '../components/sections/About/About'
+import WaitlistForm from '../components/sections/Waitlist/WaitlistForm'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <>
+    <Hero />
+    <Features />
+    <About />
+    <WaitlistForm />
+
+    </>
   )
 }
 
