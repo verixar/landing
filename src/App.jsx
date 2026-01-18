64717b454e422f10aec3d5d13eeb6d9ea3c412bf
Abdulbasit <abdulbasitkayode@gmail.com>
2026-01-18 21:33:45 +0100

    Implement main application structure with header, footer, and landing page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+
+// Components
+import TopNav from "./components/layout/Header/TopNav";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
+
+import LandingPage from "./pages/LandingPage";
+
+// Libraries
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div className="min-h-screen flex flex-col">
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: "White",
+              color: "#252B42",
+              fontFamily: "Gilroy, sans-serif",
+            },
+          }}
+        />
+        <TopNav />
+        <Header />
+        <main className="grow">
+          <LandingPage />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
